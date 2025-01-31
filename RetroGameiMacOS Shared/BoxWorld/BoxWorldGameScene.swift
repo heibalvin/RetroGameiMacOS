af8dd97c096c03947ac0fd8e05810eb4695e8381
Alvin Heib <heibalvin@gmail.com>
2025-01-31 09:53:48 +0400

    added our first Sprite

--- RetroGameiMacOS Shared/BoxWorld/BoxWorldGameScene.swift
+++ RetroGameiMacOS Shared/BoxWorld/BoxWorldGameScene.swift
@@ -7,7 +7,17 @@
 
 import SpriteKit
 
+enum BoxWorldType {
+    case none
+    case floor
+    case target
+    case wall
+    case box
+    case player
+}
+
 class BoxWorldGameScene: SKScene {
+    let atlas = SKTextureAtlas(named: "boxworld")
     
     class func newGameScene() -> BoxWorldGameScene {
         let scene = BoxWorldGameScene(size: CGSize(width: 480, height: 420))
@@ -16,5 +26,13 @@ class BoxWorldGameScene: SKScene {
         scene.scaleMode = .aspectFit
         
         return scene
+    }
+    
+    override func didMove(to view: SKView) {
+        let sprite = SKSpriteNode(texture: atlas.textureNamed("wall"))
+        sprite.name = "wall"
+        sprite.position = CGPoint(x: 7 * 30, y: 3 * 30)
+        sprite.anchorPoint = .zero
+        addChild(sprite)
     }
 }
